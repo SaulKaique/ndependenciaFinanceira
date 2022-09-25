111b6fe5441f4ece50daa71071dfb9fcf4d2de74
saulkaique <saul.silva@etec.sp.gov.br>
2022-09-25 15:50:15 -0300

    Nao consegui fazer muitas alteraçoes porque nao tive muitas ideias de como poderia mudar entao ficou assim espero que esteja bom assim

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿decimal gastosMensais, ValorAInvestir;
+double valoraserdividido;
+
+Console.WriteLine("--- Vivendo de dividendos ---\n");
+
+Console.Write("Gastos mensais (em R$)....: ");
+gastosMensais = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%)...: ");
+valoraserdividido = Convert.ToDouble(Console.ReadLine());
+
+ValorAInvestir = gastosMensais / Convert.ToDecimal(valoraserdividido / 100);
+
+Console.WriteLine($"\nPara receber {gastosMensais:C} por mês com rendimentos de {valoraserdividido:N1}% você precisa ter investido {ValorAInvestir:C}.");
